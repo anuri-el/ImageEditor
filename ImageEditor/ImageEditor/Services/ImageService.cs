b0d74bc89d4d35f47ac83d8fcc84be8ec4cf883a
anuri-el <sofia.poteychuk@gmail.com>
2025-11-23 18:02:02 +0200

    Add "Open Image"

---
+++ ImageEditor/ImageEditor/Services/ImageService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+
+namespace ImageEditor.Services
+{
+    public class ImageService
+    {
+        public BitmapImage? OpenImage()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp;*.gif;*.tiff";
+
+            if (dialog.ShowDialog() != true)
+                return null;
+
+            try
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad; // важливо: файл не блокується
+                bitmap.UriSource = new Uri(dialog.FileName);
+                bitmap.EndInit();
+                bitmap.Freeze(); // для потокобезпеки
+
+                return bitmap;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ImageEditor/ImageEditor/ViewModels/EditorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ImageEditor.Commands;
+using ImageEditor.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+
+namespace ImageEditor.ViewModels
+{
+    public class EditorViewModel : INotifyPropertyChanged
+    {
+        private readonly ImageService _imageService = new();
+
+        private BitmapImage? _loadedImage;
+        public BitmapImage? LoadedImage
+        {
+            get => _loadedImage;
+            set
+            {
+                _loadedImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand OpenImageCommand { get; }
+
+        public EditorViewModel()
+        {
+            OpenImageCommand = new RelayCommand(_ => OpenImage());
+        }
+
+        private void OpenImage()
+        {
+            var bmp = _imageService.OpenImage();
+            if (bmp != null)
+                LoadedImage = bmp;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
